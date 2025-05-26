ffe0c73d41acc8cf70d0ddfe2980f1c0820bbf8e
behrooz bozorg chami <behroozbc@outlook.com>
2025-05-26 22:02:18 +0200

    ad JsonPropertyNames

--- src/DuckDuckGoDotNet/Search/SearchItem.cs
+++ src/DuckDuckGoDotNet/Search/SearchItem.cs
@@ -1,12 +1,18 @@
+using System.Text.Json.Serialization;
+
 namespace DuckDuckGoDotNet.Search;
+
 public class BaseSearchItem
 {
+    [JsonPropertyName("title")]
     public string Title { get; init; }
 
 }
 public class TextSearchItem : BaseSearchItem
 {
+    [JsonPropertyName("href")]
     public Uri Href { get; init; }
+    [JsonPropertyName("description")]
     public string Description { get; init; }
 }
 public class ImageSearchItem : BaseMediaSearchItem
@@ -17,12 +23,17 @@ public class ImageSearchItem : BaseMediaSearchItem
 }
 public class BaseMediaSearchItem : BaseSearchItem
 {
+    [JsonPropertyName("image")]
     public Uri Image { get; init; }
+    [JsonPropertyName("url")]
     public Uri URL { get; init; }
+    [JsonPropertyName("source")]
     public string Source { get; init; }
 }
 public class NewsSearchItem : BaseMediaSearchItem
 {
+    [JsonPropertyName("date")]
     public DateTimeOffset Date { get; init; }
+    [JsonPropertyName("body")]
     public string Body { get; init; }
 }
